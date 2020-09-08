cba48d0c552d11bcd326935882078365cf69ff2f
Matheus Justino <matheussjusttino@gmail.com>
2020-09-08 16:12:31 -0300

    09. portals

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import PortalDemo from './components/PortalDemo'
 import './App.css';
-import ForwardParent from './components/ForwardParent';
 
 function App() {
   console.log('App is render');
   return (
-    <ForwardParent />
+    <PortalDemo />
   )
 }
 
--- src/components/ForwardChild.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ForwardChild = React.forwardRef((props, ref) => {
-    return (
-        <input ref={ref} />
-    );
-})
-
-export default ForwardChild;
